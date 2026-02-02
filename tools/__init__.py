d808d5a5d82f419491c9c541682953d4357b4e5b
Onyeka Nwokike <nwokikeonyeka@gmail.com>
2026-02-02 16:32:21 +0100

    Add additional search functions to __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -11,7 +11,12 @@ from .browser_tools import (
     get_page_content,
     scroll_page,
 )
-from .search_tools import search_jobs
+from .search_tools import (
+    search_jobs, 
+    search_web, 
+    search_company_info, 
+    search_job_boards
+)
 
 __all__ = [
     "navigate_to_url",
@@ -21,4 +26,7 @@ __all__ = [
     "get_page_content",
     "scroll_page",
     "search_jobs",
+    "search_web",
+    "search_company_info",
+    "search_job_boards"
 ]
